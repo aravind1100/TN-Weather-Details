e56ac240aa27413877722c3c07603ee076bd83ec
aravind1100 <aravindavd1999@gmail.com>
2024-05-08 21:35:41 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const row = document.getElementById("row");
 const cities =
-  fetch(`https://api.geonames.org/searchJSON?country=IN&featureClass=P&maxRows=1000&username=example123
+  fetch(`https://secure.geonames.org/searchJSON?country=IN&featureClass=P&maxRows=1000&username=example123
 `);
 cities
   .then((dat) => dat.json())
